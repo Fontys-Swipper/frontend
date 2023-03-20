081ee1d1174bd596c293922d5f21c7515d3ebd60
JonneM <jonne.mustajarvi@gmail.com>
2023-03-20 16:50:08 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+
+  return (
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  );
+}
+
+export default App;
