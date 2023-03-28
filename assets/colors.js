6962c3b93ab93664cc773096409db0d03febfb47
nismq <jereniss.00@gmail.com>
2023-03-28 15:00:36 +0300

    Colors file added

---
+++ assets/colors.js
@@ -0,0 +1,9 @@
+
+export const COLORS = {
+    primary: '#FAAF6C',
+    secondary: '#BAD36D',
+    background: 'F8F3DF',
+    background2: 'E3CD9C',
+    white: '#2D2D2D',
+    black: '#FFFEF9',
+  }
