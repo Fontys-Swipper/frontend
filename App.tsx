2fdb3828722e3f0828633aae392d3ea5b9d63ac4
JonneM <jonne.mustajarvi@gmail.com>
2023-03-28 14:30:46 +0300

    Added couple dependencies and simple home screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/views/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
 
   return (
     <NavigationContainer>
-      
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "Home"
+          component={Home}
+          options={{title:"Home"}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
 
 export default App;
