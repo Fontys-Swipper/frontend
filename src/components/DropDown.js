6da431aa689e80db3cd3019a72a0cedcc96a894f
Rajat <rajatrijal26@gmail.com>
2023-04-05 14:14:57 +0300

    dropdown-menu

---
+++ src/components/DropDown.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {SelectList} from 'react-native-dropdown-select-list';
+import {COLORS} from '../../assets/colors';
+import {View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+const DropDown = ({choice = [], placehoder = ''}) => {
+  const [selected, setSelected] = React.useState('');
+
+  return (
+    <View>
+      <SelectList
+        placeholder={placehoder}
+        search={false}
+        dropdownStyles={{
+          alignSelf: 'flex-end',
+          width: '30%',
+          backgroundColor: Colors.backgroundColor,
+          borderColor: COLORS.primary,
+          borderWidth: 0,
+          marginRight: '14%',
+        }}
+        dropdownItemStyles={{
+          padding: 3,
+          borderBottomColor: COLORS.primary,
+        }}
+        dropdownTextStyles={{
+          textAlign: 'center',
+          borderBottomWidth: 0.5,
+          borderColor: COLORS.primary,
+          paddingBottom: 5,
+        }}
+        boxStyles={{
+          borderColor: COLORS.primary,
+          marginLeft: '12%',
+          marginRight: '12%',
+          borderWidth: 0,
+          borderBottomWidth: 1,
+          borderRadius: 0,
+        }}
+        placehoderStyle={{
+          fontFamily: 'RobotoSlab-Regular',
+          fontSize: 13,
+          marginLeft: '15%',
+          fontWeight: 'bold',
+          textDecorationColor: COLORS.black,
+        }}
+        setSelected={val => setSelected(val)}
+        data={choice}
+        save={choice}
+      />
+    </View>
+  );
+};
+
+export default DropDown;
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,11 +3,13 @@ import {Text, View} from "react-native"
 
 const Home = ({navigation}) => {
     return (
-        <View>
-            <Text>
-                This is home screen
-            </Text>
-        </View>        
+      <View>
+        <Text>
+            
+        </Text>
+
+      </View>
+        
     );
 }
 
