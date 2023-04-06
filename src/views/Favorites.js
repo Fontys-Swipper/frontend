9a7ef406a8c7c28cb622851a806d32ac030b99b5
Rajat <rajatrijal26@gmail.com>
2023-04-06 14:49:55 +0300

    dropdown

---
+++ src/views/Favorites.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import {Text, View} from "react-native"
+
+const Favorites = ({navigation}) => {
+    return (
+        <View>
+            <Text>
+                This is favorites screen
+            </Text>
+        </View>        
+    );
+}
+
+export default Favorites;
