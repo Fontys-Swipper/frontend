2abf268ec18089a1443f03d53f727631a4022ebb
Marion <marionroussel1@hotmail.fr>
2023-04-18 13:18:14 +0300

    Finished topBar, added it to the following pages: Feed, Match, Favorites and Chat. For now the navigation isn't working properly, but I still managed to make it work once, so should be fixable. Created also pages: Profile.js and Settings.js as they are needed in the topBar. Didn't add anything for Add Listing as I'm not sure how Rajat will make it work. Any changes can be done regarding the Profile and Settings pages ofc, it's very easy fix to change the destination of the page.

---
+++ src/views/Settings.js
@@ -0,0 +1,16 @@
+import React from "react"; 
+import {Text, View, ToastAndroid, Button} from "react-native"
+import { COLORS } from "../../assets/colors.js"
+
+const Settings = ({navigation}) => {
+
+    return (
+        <View>
+            <Text>
+                This is settings screen
+            </Text> 
+        </View>      
+    );
+}
+
+export default Settings;
