7a93df2af4951cd6abd4254698b5eb9b284c195c
nismq <jereniss.00@gmail.com>
2023-04-26 10:08:16 +0300

    Typo fix

--- src/views/Feed.js
+++ src/views/Feed.js
@@ -161,7 +161,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        wlisting_idth: 'animal_100%',
+        width: '100%',
         height: '100%',
         backgroundColor: COLORS.black + '99'
     },
