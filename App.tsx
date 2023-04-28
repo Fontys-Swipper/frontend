5e7a53fdf54730cff9dd2c079c2e5389492ce0db
WellyBelly <89990168+WellyBelly@users.noreply.github.com>
2023-04-28 12:31:06 +0300

    Finished preliminary detailView

--- App.tsx
+++ App.tsx
@@ -1,24 +1,28 @@
-
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/views/Home';
+import DetailView from './src/views/DetailView';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {/* <Stack.Screen
           name = "Home"
           component={Home}
           options={{title:"Home"}}
+        /> */}
+        <Stack.Screen
+          name="DetailView"
+          component={DetailView}
+          options={{title: 'DetailView'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,19 +1,15 @@
-import React from "react"; 
+import React from 'react';
 
-import {Text, View, ToastAndroid, Button} from "react-native"
+import {Text, View, ToastAndroid, Button} from 'react-native';
 
-import { COLORS } from "../../assets/colors.js"
-
+import {COLORS} from '../../assets/colors.js';
 
 const Home = ({navigation}) => {
-
-    return (
-        <View>
-            <Text>
-                This is home screen
-            </Text> */}
-        </View>      
-    );
-}
+  return (
+    <View>
+      <Text>This is home screen</Text>
+    </View>
+  );
+};
 
 export default Home;
