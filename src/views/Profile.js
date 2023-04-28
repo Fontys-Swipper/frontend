fb03d92e0066f5bf898182ab113564894a633400
WellyBelly <89990168+WellyBelly@users.noreply.github.com>
2023-04-28 13:05:53 +0300

    preliminary profile version

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,16 +1,70 @@
-import React from "react"; 
-import {Text, View, ToastAndroid, Button} from "react-native"
-import { COLORS } from "../../assets/colors.js"
+import React from 'react';
+import {Text, View, ScrollView, ToastAndroid, Button} from 'react-native';
+import {COLORS} from '../../assets/colors.js';
+import TopBar from '../components/TopBar.js';
+
+import Btn_back_arrow from '../components/buttons/Btn_back_arrow.js';
+
+import image1 from '../../assets/images/cat-g2ff4963cc_1920.jpg';
+import image2 from '../../assets/images/malinois-g4dd9f780d_1920.jpg';
+import image3 from '../../assets/images/dog_image.jpg';
+
+const dummy = [
+  {
+    listing_id: 1,
+    animal_species: 'Cat',
+    animal_image_link: image1,
+    animal_name: 'Mr Twinkles',
+    price: 250,
+    timeOfAdding: '01/01/2023',
+  },
+  {
+    listing_id: 2,
+    animal_species: 'Dog',
+    animal_image_link: image2,
+    animal_name: 'Medor',
+    price: 300,
+    timeOfAdding: '12/04/2023',
+  },
+  {
+    listing_id: 3,
+    animal_species: 'Dog',
+    animal_image_link: image3,
+    animal_name: 'Markus',
+    price: 200,
+    timeOfAdding: '24/04/2023',
+  },
+];
 
 const Profile = ({navigation}) => {
+  const [listings, setListings] = useState([]);
 
-    return (
-        <View>
-            <Text>
-                This is profile screen
-            </Text> 
-        </View>      
-    );
-}
+  setListings(dummy);
+
+  return (
+    <View>
+      <ScrollView>
+        <TopBar />
+        <Btn_back_arrow />
+        <Text>Max Mustermann</Text>
+        <Text>City</Text>
+        <Text>Living Space</Text>
+        <Text>Description</Text>
+        <Text>Has Pet (yes or no)</Text>
+        <Text>Has Garden (yes or no)</Text>
+        <Text>Own listings</Text>
+        {listings.map(item => (
+          <Feedcard
+            key={item.listing_id}
+            image={item.animal_image_link}
+            name={item.animal_name}
+            price={item.price}
+            timeOfAdding={item.timeOfAdding}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
 export default Profile;
