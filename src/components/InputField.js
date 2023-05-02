0b9e309c5a1d93dc1c2a9151a5ab7a97e2513bd1
nismq <jereniss.00@gmail.com>
2023-05-02 20:15:21 +0300

    Added multiline option

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {View, StyleSheet, TextInput, Text} from 'react-native';
 import { COLORS } from '../../assets/colors'
 
-const InputField = ({text_title="", text_color=COLORS.black, onChangeText, defaultValue, value, icon }) => {
+const InputField = ({text_title="", text_color=COLORS.black, onChangeText, defaultValue, value, icon, multiline = false, numberOfLines }) => {
   const [text, setText] = React.useState('');
 
 
   return (
     <View style={styles.container}>
       <Text style={[styles.text, styles.text.color = text_color]}>{text_title}</Text>
-      <View style={styles.innerContainer}>
+      <View style={styles.innerContainer({multiline})}>
         <TextInput
         required
         style={styles.input}
@@ -17,6 +17,9 @@ const InputField = ({text_title="", text_color=COLORS.black, onChangeText, defau
         value={value}
         onChangeText={onChangeText}
         defaultValue={defaultValue}
+        multiline = {multiline}
+        numberOfLines={4}
+        textAlignVertical={multiline? 'top': 'center'}
         />
         {/* If no icon is given the margins are 0 so whole space is available for text input*/}
         <View style={[styles.iconContainer, icon ? styles.iconContainer.marginHorizontal = 5 : styles.iconContainer.marginHorizontal = 0]}>
@@ -33,20 +36,26 @@ const styles = StyleSheet.create({
     width:258,
     borderBottomWidth:1,
     borderBottomColor:COLORS.primary, 
-    margin: 10,
+    margin: 5,
+    paddingBottom: 5,
+    borderTopEndRadius: 6,
+    borderTopStartRadius: 6,
+
   },
   input: {
     fontFamily:"Roboto-Light",
     fontSize: 15,
     padding: 0,
     flex: 1,
+    height: '100%',
   },
-  innerContainer: {
-    height: 43,
+  innerContainer: ({multiline}) => [{
+    height: multiline? 58: 35,
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal:15,
-  },
+    paddingVertival: 10,
+  }],
   iconContainer: {
     marginHorizontal: 6,
     alignItems:'center',
@@ -56,7 +65,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto-Light',
     fontSize: 15,
     paddingLeft: 15,
-    margin: 0,
+    marginBottom: 5,
     color: COLORS.black, 
  },
 });
