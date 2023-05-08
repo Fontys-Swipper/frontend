f7a1a97db6f00f6b6234709e66610b8ded0be6cb
RajatRijal <rajatrijal26@gmail.com>
2023-05-08 14:01:06 +0300

    endpoints

--- src/views/AddListingPageThree.js
+++ src/views/AddListingPageThree.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, SafeAreaView} from 'react-native';
 import TextHeading from './TextHeading';
 import SelectImage from '../components/SelectImage';
 import Btn_solid_regular from '../components/buttons/Btn_solid_regular';
 import Btn_back_arrow from '../components/buttons/Btn_back_arrow';
+import InputField from '../components/InputField';
 
-import {useState} from 'react';
 import TopBar from '../components/TopBar';
 import NavigationBarContainer from '../components/NavigationBarContainer';
 import DescriptionBox from '../components/DescriptionBox';
 import {COLORS} from '../../assets/colors';
 import Btn_solid_small from '../components/buttons/Btn_solid_small';
+
 const AddListingPageThree = ({navigation}) => {
   const [selectedImages, setSelectedImages] = useState([]);
+  const [url, setUrl] = useState('');
+
+  const handleAddListing = async () => {
+    try {
+      const response = await fetch(
+        'https://swipperresource.azurewebsites.net/api/listing',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            images: selectedImages,
+            url,
+          }),
+        },
+      );
+      const data = await response.json();
+      console.log(data); // handle response data
+    } catch (error) {
+      console.error(error); // handle error
+    }
+  };
 
   return (
     <SafeAreaView
@@ -27,7 +51,6 @@ const AddListingPageThree = ({navigation}) => {
       <View style={{flex: 1}}>
         <TopBar style={{position: 'absolute'}} />
         <Btn_back_arrow onPress={() => navigation.navigate('AddListing2')} />
-
         <View>
           <TextHeading text_title="Add Listing" />
           <SelectImage
@@ -35,13 +58,19 @@ const AddListingPageThree = ({navigation}) => {
             setSelectedImages={setSelectedImages}
           />
         </View>
-        {/* eslint-disable-line react-native/no-inline-styles */}
+        <View>
+          <InputField
+            text_title="Input the url"
+            value={url}
+            onChangeText={setUrl}
+          />
+        </View>
         <View
           style={{alignSelf: 'center', marginTop: 600, position: 'absolute'}}>
-          <Btn_solid_regular title="Add" />
+          <Btn_solid_regular title="Add" onPress={handleAddListing} />
         </View>
       </View>
-      {/* <NavigationBarContainer style={{position: 'absolute'}} /> */}
+      {/* <NavigationBarContainer style={{ position: 'absolute' }} /> */}
     </SafeAreaView>
   );
 };
