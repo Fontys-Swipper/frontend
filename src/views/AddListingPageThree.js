178a6c301d0e204c188ec9187e0c03c8c984f52d
RajatRijal <rajatrijal26@gmail.com>
2023-05-09 14:50:34 +0300

    posting

--- src/views/AddListingPageThree.js
+++ src/views/AddListingPageThree.js
@@ -1,85 +1,85 @@
-import React, {useState} from 'react';
-import {View, SafeAreaView} from 'react-native';
-import TextHeading from './TextHeading';
-import SelectImage from '../components/SelectImage';
-import Btn_solid_regular from '../components/buttons/Btn_solid_regular';
-import Btn_back_arrow from '../components/buttons/Btn_back_arrow';
-import InputField from '../components/InputField';
-import AddListingPageTwo from './AddListingPageTwo';
-import TopBar from '../components/TopBar';
-import NavigationBarContainer from '../components/NavigationBarContainer';
-import DescriptionBox from '../components/DescriptionBox';
-import {COLORS} from '../../assets/colors';
-import Btn_solid_small from '../components/buttons/Btn_solid_small';
+// import React, {useState} from 'react';
+// import {View, SafeAreaView} from 'react-native';
+// import TextHeading from './TextHeading';
+// import SelectImage from '../components/SelectImage';
+// import Btn_solid_regular from '../components/buttons/Btn_solid_regular';
+// import Btn_back_arrow from '../components/buttons/Btn_back_arrow';
+// import InputField from '../components/InputField';
+// import AddListingPageTwo from './AddListingPageTwo';
+// import TopBar from '../components/TopBar';
+// import NavigationBarContainer from '../components/NavigationBarContainer';
+// import DescriptionBox from '../components/DescriptionBox';
+// import {COLORS} from '../../assets/colors';
+// import Btn_solid_small from '../components/buttons/Btn_solid_small';
 
-const AddListingPageThree = ({navigation}) => {
-  const [selectedImages, setSelectedImages] = useState([]);
-  const [url, setUrl] = useState('');
+// const AddListingPageThree = ({navigation}) => {
+//   const [selectedImages, setSelectedImages] = useState([]);
+//   const [url, setUrl] = useState('');
 
-  const handleAddListing = async () => {
-    try {
-      const response = await fetch(
-        'https://swipperresource.azurewebsites.net/api/listing',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            images: selectedImages,
-            url,
-            name,
-            gender,
-            type,
-            kind,
-            species,
-            age,
-            size,
-          }),
-        },
-      );
-      const data = await response.json();
-      console.log(data); // handle response data
-    } catch (error) {
-      console.error(error); // handle error
-    }
-  };
+//   const handleAddListing = async () => {
+//     try {
+//       const response = await fetch(
+//         'https://swipperresource.azurewebsites.net/api/listing',
+//         {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({
+//             images: selectedImages,
+//             url,
+//             name,
+//             gender,
+//             type,
+//             kind,
+//             species,
+//             age,
+//             size,
+//           }),
+//         },
+//       );
+//       const data = await response.json();
+//       console.log(data); // handle response data
+//     } catch (error) {
+//       console.error(error); // handle error
+//     }
+//   };
 
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        position: 'absolute',
-        height: '100%',
-        width: '100%',
-        justifyContent: 'space-around',
-        backgroundColor: COLORS.white,
-      }}>
-      <View style={{flex: 1}}>
-        <TopBar style={{position: 'absolute'}} />
-        <Btn_back_arrow onPress={() => navigation.navigate('AddListing2')} />
-        <View>
-          <TextHeading text_title="Add Listing" />
-          <SelectImage
-            selectedImages={selectedImages}
-            setSelectedImages={setSelectedImages}
-          />
-        </View>
-        <View>
-          <InputField
-            text_title="Input the url"
-            value={url}
-            onChangeText={setUrl}
-          />
-        </View>
-        <View
-          style={{alignSelf: 'center', marginTop: 600, position: 'absolute'}}>
-          <Btn_solid_regular title="Add" onPress={handleAddListing} />
-        </View>
-      </View>
-      {/* <NavigationBarContainer style={{ position: 'absolute' }} /> */}
-    </SafeAreaView>
-  );
-};
+//   return (
+//     <SafeAreaView
+//       style={{
+//         flex: 1,
+//         position: 'absolute',
+//         height: '100%',
+//         width: '100%',
+//         justifyContent: 'space-around',
+//         backgroundColor: COLORS.white,
+//       }}>
+//       <View style={{flex: 1}}>
+//         <TopBar style={{position: 'absolute'}} />
+//         <Btn_back_arrow onPress={() => navigation.navigate('AddListing2')} />
+//         <View>
+//           <TextHeading text_title="Add Listing" />
+//           <SelectImage
+//             selectedImages={selectedImages}
+//             setSelectedImages={setSelectedImages}
+//           />
+//         </View>
+//         <View>
+//           <InputField
+//             text_title="Input the url"
+//             value={url}
+//             onChangeText={setUrl}
+//           />
+//         </View>
+//         <View
+//           style={{alignSelf: 'center', marginTop: 600, position: 'absolute'}}>
+//           <Btn_solid_regular title="Add" onPress={handleAddListing} />
+//         </View>
+//       </View>
+//       {/* <NavigationBarContainer style={{ position: 'absolute' }} /> */}
+//     </SafeAreaView>
+//   );
+// };
 
-export default AddListingPageThree;
+// export default AddListingPageThree;
--- src/views/AddListingPageTwo.js
+++ src/views/AddListingPageTwo.js
@@ -1,80 +1,80 @@
-import React, {useState} from 'react';
-import {View, Text, KeyboardAvoidingView} from 'react-native';
-import DropDown from '../components/DropDown';
-import InputField from '../components/InputField';
-import Btn_solid_regular from '../components/buttons/Btn_solid_regular';
-import NavigationBarContainer from '../components/NavigationBarContainer';
-import {COLORS} from '../../assets/colors';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import DescriptionBox from '../components/DescriptionBox';
-import TextHeading from './TextHeading';
-import TopBar from '../components/TopBar';
-import Btn_back_arrow from '../components/buttons/Btn_back_arrow';
-import {ScrollView} from 'react-native-gesture-handler';
+// import React, {useState} from 'react';
+// import {View, Text, KeyboardAvoidingView} from 'react-native';
+// import DropDown from '../components/DropDown';
+// import InputField from '../components/InputField';
+// import Btn_solid_regular from '../components/buttons/Btn_solid_regular';
+// import NavigationBarContainer from '../components/NavigationBarContainer';
+// import {COLORS} from '../../assets/colors';
+// import {SafeAreaView} from 'react-native-safe-area-context';
+// import DescriptionBox from '../components/DescriptionBox';
+// import TextHeading from './TextHeading';
+// import TopBar from '../components/TopBar';
+// import Btn_back_arrow from '../components/buttons/Btn_back_arrow';
+// import {ScrollView} from 'react-native-gesture-handler';
 
-const AddListingPageTwo = ({navigation}) => {
-  const [gender, setGender] = useState('');
-  const [size, setSize] = useState('');
+// const AddListingPageTwo = ({navigation}) => {
+//   const [gender, setGender] = useState('');
+//   const [size, setSize] = useState('');
 
-  return (
-    <SafeAreaView
-      style={{
-        height: '100%',
-        width: 'auto',
-        flex: 1,
-        position: 'relative',
-        backgroundColor: COLORS.white,
-      }}>
-      <View>
-        <TopBar style={{position: 'absolute'}} />
-        <Btn_back_arrow onPress={() => navigation.navigate('AddListing1')} />
-      </View>
-      <ScrollView>
-        <View
-          style={{
-            flex: 1,
-          }}>
-          <View>
-            <TextHeading text_title="Add Listing" />
-          </View>
+//   return (
+//     <SafeAreaView
+//       style={{
+//         height: '100%',
+//         width: 'auto',
+//         flex: 1,
+//         position: 'relative',
+//         backgroundColor: COLORS.white,
+//       }}>
+//       <View>
+//         <TopBar style={{position: 'absolute'}} />
+//         <Btn_back_arrow onPress={() => navigation.navigate('AddListing1')} />
+//       </View>
+//       <ScrollView>
+//         <View
+//           style={{
+//             flex: 1,
+//           }}>
+//           <View>
+//             <TextHeading text_title="Add Listing" />
+//           </View>
 
-          <View style={{marginBottom: 31}}>
-            <DropDown
-              placehoder="Size"
-              choice={['Tiny', 'Small', 'Medium', 'Large']}
-              setSelected={val => setSize(val)}
-            />
-          </View>
-          <View style={{marginBottom: 31}}>
-            <DropDown
-              placehoder="Gender"
-              choice={['Male', 'Female', 'Unknown']}
-              setSelected={val => setGender(val)}
-            />
-          </View>
-          <View style={{marginBottom: 28}}>
-            <DescriptionBox
-              palceholder="Give some information about animal...."
-              text_title="Description"
-            />
-          </View>
-          <View style={{alignSelf: 'center'}}>
-            <Btn_solid_regular
-              title="Next"
-              // eslint-disable-next-line no-undef
-              onPress={() =>
-                navigation.navigate('AddListing3', {
-                  setGender,
-                  setSize,
-                })
-              }
-            />
-          </View>
-        </View>
-      </ScrollView>
-      {/* <NavigationBarContainer style={{position: 'absolute'}} /> */}
-    </SafeAreaView>
-  );
-};
+//           <View style={{marginBottom: 31}}>
+//             <DropDown
+//               placehoder="Size"
+//               choice={['Tiny', 'Small', 'Medium', 'Large']}
+//               setSelected={val => setSize(val)}
+//             />
+//           </View>
+//           <View style={{marginBottom: 31}}>
+//             <DropDown
+//               placehoder="Gender"
+//               choice={['Male', 'Female', 'Unknown']}
+//               setSelected={val => setGender(val)}
+//             />
+//           </View>
+//           <View style={{marginBottom: 28}}>
+//             <DescriptionBox
+//               palceholder="Give some information about animal...."
+//               text_title="Description"
+//             />
+//           </View>
+//           <View style={{alignSelf: 'center'}}>
+//             <Btn_solid_regular
+//               title="Next"
+//               // eslint-disable-next-line no-undef
+//               onPress={() =>
+//                 navigation.navigate('AddListing3', {
+//                   setGender,
+//                   setSize,
+//                 })
+//               }
+//             />
+//           </View>
+//         </View>
+//       </ScrollView>
+//       {/* <NavigationBarContainer style={{position: 'absolute'}} /> */}
+//     </SafeAreaView>
+//   );
+// };
 
-export default AddListingPageTwo;
+// export default AddListingPageTwo;
